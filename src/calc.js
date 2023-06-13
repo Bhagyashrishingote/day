563b678365166ccc0ae9a79edadfcf2f84d85a90
bhagyashri <bhagyashrishingote244@gmail.com>
2023-06-13 23:29:08 +0530

    setTimeout

--- src/calc.js
+++ src/calc.js
@@ -2,4 +2,3 @@ function sum(n1,n2){
     return n1+n2;
 
 }
-export{sum};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import {sum} from "./calc.js";
+/*import {sum} from "./calc.js";
 //import fs from "fs";
 import {readFileSync} from "node:fs";
 let filePath= "C:/Users/91702/Desktop/WPT MODULE/programs/day/package.json";
-//let fileData=readFileSync(filePath);
+let fileData=readFileSync(filePath);
 let fileData=readFileSync(filePath, {encoding:"utf-8"});
-console.log(fileData);
+console.log(fileData);*/
+/*import { readFileSync, writeFileSync } from "node:fs";
+let filePath= "C:/Users/91702/Desktop/output.txt";
+//let data="hello world good morning";
+let data =`Save your favorite articles to read
+ offline, sync your reading lists across devices 
+ and customize your reading experience with the
+  official Wikipedia app.`;
+writeFileSync(filePath, data);
+console.log("write successfully.......!!!!!!!");*/
 
+console.log("hello world 1");
+setTimeout(() =>console.log("hello world 2"),1000)
+console.log("hello world 3");
+console.log("hello world 4");
+console.log("hello world 5");
+console.log("hello world 6");
