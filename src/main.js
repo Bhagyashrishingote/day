77ff6dbf6e0014603cfe7f49a94da0fc4c3c269b
bhagyashri <bhagyashrishingote244@gmail.com>
2023-06-14 13:20:13 +0530

    promice read file

--- src/main.js
+++ src/main.js
@@ -1,43 +1,8 @@
-/* async function sum(n1,n2){
-  return n1+n2;
-}
-let op=sum(1,1);
-console.log(op);  */
-
-/*async function hello(){
- console.log("hello 1");
-}
-hello();
-
-console.log("hello 2");
-
-console.log("hello 3");         
-
-/*function hello(){
- console.log("hello 1");
-}
-console.log("hello 2");
-hello();
-console.log("hello 3"); */
-
-/*function hello(){
- console.log("hello 1");
-}
-console.log("hello 2");
-hello();
-console.log("hello 3"); */
-
-/*async function sum(n1,n2){
-  return n1+n2;
-}
-let result=sum(8,1);
-console.log(result) ;             /*op-> promice{2}*/
-
-async function sum(n1,n2){
-  return n1+n2;
-}
+import {readFile} from "node:fs/promises";
 async function main(){
-let result = await sum(1,3);
-console.log(resilt) 
+  let filePath="C:/Users/91702/Desktop/WPT MODULE/programs/day/package.json"
+  let fileData= await readFile(filePath, {encoding: "utf-8"});
+  console.log(fileData);
+
 }
 main();
